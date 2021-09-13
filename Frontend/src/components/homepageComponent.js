2147c8cdbd8cd967db0e7e80c88178d86b09d9f7
giovanna_02 <francheska1994guanipa@gmail.com>
2021-09-13 08:20:53 -0500

    jhonatan

--- Frontend/src/components/homepageComponent.js
+++ Frontend/src/components/homepageComponent.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 
-class homepageComponent extends Component {
+ class homepageComponent extends Component {
+   construtor(props){
+super(props)
+   
+
+     
+   }
   render() {
     return (
       <div>
